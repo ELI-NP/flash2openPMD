fc4b7569e2e9b16995496779b9bba03ccc2271e6
StevE-Ong <jianfuh.ong@eli-np.ro>
2023-08-10 16:45:29 +0300

    Find file with iteration only

--- src/Convert.py
+++ src/Convert.py
@@ -23,16 +23,22 @@ user = pwd.getpwuid(os.getuid())[0]
 sys.path.append(rf"{user}/Flash2OpenPMD/src")
 import yt
 import numpy as np
-
+import glob
 from flash2openpmd import Convert
 
 
-run_directory = rf"~/Flash2OpenPMD/Data"  #path of the flash2openpmd script you have on your PC
+run_directory = input("Enter path of the simulation data:")  # path of the simulation data
 
 #filename = "lasslab_hdf5_plt_cnt_0043"
-filename = input("Enter the name of the file you want to convert: ")
-ts = Convert(run_directory,filename)
+        
+step = input("Iteration: ")
 
-density = ts.get_data(x_min=0,y_min=0) #minimum valueas for x and y
+filename = glob.glob1(run_directory,"*_hdf5_plt_cnt_"+str(int(step)).zfill(4))
+
+print(filename[0])
+
+ts = Convert(run_directory,filename[0])
+
+density = ts.get_data() #minimum valueas for x and y
 
 ts.write2openpmd(density)
